e7ba4c3fcdf0ae5480a2e796c8d072ebb4093f33
Venkat-Penmatsa <50638928+Venkat-Penmatsa@users.noreply.github.com>
2023-01-23 18:01:11 +0100

    mobility changes

---
+++ src/app/services/mobility.service.ts
@@ -0,0 +1,29 @@
+import { DOCUMENT } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HostNameServiceService } from './host-name-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MobilityService {
+
+  private baseUrl = "https://" + this.document.location.hostname + ':' + this.document.location.port + "/admin-services";
+
+  constructor(private http: HttpClient, private hostNameServiceService: HostNameServiceService, @Inject(DOCUMENT) private document: Document) {
+    this.baseUrl = hostNameServiceService.getHostname();
+  }
+
+
+  getMobilityDetails(empId: any, selectedDate: any): Observable<any> {
+    const headers = { 'Content-type': 'application/json' };
+    return this.http.get(`${this.baseUrl}/mobility/getMobilityDetails/` + empId + '/' + selectedDate, { headers })
+  }
+
+  applyMobility(applyMob: any): Observable<any> {
+    const headers = { 'Content-type': 'application/json' };
+    return this.http.post(`${this.baseUrl}/mobility/saveMobility/`, applyMob, { headers })
+  }
+  
+}
