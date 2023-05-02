3a4ce2fb79dbcc5d66b9e96332a9f7f31a557b6c
Venkat-Penmatsa <50638928+Venkat-Penmatsa@users.noreply.github.com>
2023-05-02 22:53:02 +0200

    package name changes

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nish-team'),
+      dir: require('path').join(__dirname, './coverage/NEPtune'),
       subdir: '.',
       reporters: [
         { type: 'html' },
