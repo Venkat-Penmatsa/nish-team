329c01d42a879fbc77b49e8d4b46f79d4789f444
Venkat-Penmatsa <50638928+Venkat-Penmatsa@users.noreply.github.com>
2024-02-12 19:17:17 +0100

    NEPTUNE bugs

--- src/app/mockData/notification.ts
+++ src/app/mockData/notification.ts
@@ -16,4 +16,9 @@ export const NOTIFICATION: Notification[] = [
     title:
       'Any changes in employee details please send updates to SPINE team immediately',
   },
+  {
+    id: 14,
+    title:
+      'All sick leaves or any changes to leaves or medical certificates to be sent ONLY to hr@nishtech.be and cc sp@nishtech.be. No emails to other ids will be entertained',
+  },
 ];
