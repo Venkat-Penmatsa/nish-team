793089f957ca692ea08a136b08e4d2bedc69c646
Venkat-Penmatsa <161831121+venkatraju-NISH@users.noreply.github.com>
2024-10-18 16:27:25 +0200

    offbaording changes

--- src/app/constants/documentType.ts
+++ src/app/constants/documentType.ts
@@ -1,29 +1,27 @@
 export let EmployeeDocs = [
-    { code: "OL", name: "Offer Letter" },
-    { code: "CO", name: "Contract" },
-    { code: "SC", name: "Sodexo Contract" },
-    { code: "CC", name: "Car Contract" },
-    { code: "PC", name: "Employee Picture"},
-    { code: "MC", name: "Medical Certificate"},
-    { code: "SP", name: "Single Permit"},
-    { code: "OO", name: "Others" }
-  ];
+  { code: 'OL', name: 'Offer Letter' },
+  { code: 'CO', name: 'Contract' },
+  { code: 'SC', name: 'Sodexo Contract' },
+  { code: 'CC', name: 'Car Contract' },
+  { code: 'PC', name: 'Employee Picture' },
+  { code: 'MC', name: 'Medical Certificate' },
+  { code: 'SP', name: 'Single Permit' },
+  { code: 'RL', name: 'Resignation Letter' },
+  { code: 'OO', name: 'Others' },
+];
 
-  export let AssetsDocs = [
-    { code: "AP", name: "Asset Pictures" },
-    { code: "AB", name: "Asset Invoice" },
-    { code: "AW", name: "Asset Warrenty" },
-    { code: "OO", name: "Others" }
-  ];
+export let AssetsDocs = [
+  { code: 'AP', name: 'Asset Pictures' },
+  { code: 'AB', name: 'Asset Invoice' },
+  { code: 'AW', name: 'Asset Warrenty' },
+  { code: 'OO', name: 'Others' },
+];
 
-  export let CategoryDocs = [
-    { code: "OO", name: "Others" }
-  ];
+export let CategoryDocs = [{ code: 'OO', name: 'Others' }];
 
-  export let UploadDocs = [
-    { code: "EU", name: "Employees" },
-    { code: "CU", name: "Contracts" },
-    { code: "LU", name: "Leaves" },
-    { code: "AU", name: "Assets" },
-
-  ];
+export let UploadDocs = [
+  { code: 'EU', name: 'Employees' },
+  { code: 'CU', name: 'Contracts' },
+  { code: 'LU', name: 'Leaves' },
+  { code: 'AU', name: 'Assets' },
+];
